6386f25d5900d90b101033a52cb47cb9d72eb03e
59sjosan <josansejal2002@gmail.com>
2023-04-12 09:11:21 -0400

    A2

---
+++ ques2.py
@@ -0,0 +1,10 @@
+a=int(input("Starting number of organisms: "))
+per=int(input("Average daily percentage increase: "))
+b=int(input("Enter the number of days to display the final data: "))
+print("Day Approximate \t Population")
+print("----------------------------------------------------")
+for x in range(b):
+	print(x+1,"\t\t\t",a)
+	#Increasing value of population
+	a=a+(per*a/100)
+  
